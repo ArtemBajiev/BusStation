51483329447fdfc73c1f0efb38a76684fa305d7d
Артем Баджиев <95535835+ArtemBajiev@users.noreply.github.com>
2023-04-24 13:59:53 +0300

    .

---
+++ src/router/index.js
@@ -0,0 +1,54 @@
+import {createRouter, createWebHistory} from 'vue-router'
+import Main from '../views/Main.vue'
+import Races from '../views/Races.vue'
+import SeatPage from '../views/SeatPage.vue'
+import form from '../views/form.vue'
+import AuthorizationPage from '../views/AuthorizationPage.vue'
+import PersonalAccount from '../views/PersonalAccount.vue'
+import Faq from '../views/Faq.vue'
+
+
+const routes = [
+  {
+    path: '/',
+    name: 'Main',
+    component: Main
+  },
+  {
+    path: '/Faq',
+    name: 'Faq',
+    component: Faq
+  },
+  {
+    path: '/races/:dispatch_id/:dispatch_name/:arrival_id/:arrival_name/:date',
+    name: 'Races',
+    component: Races
+  },
+  {
+    path: '/seats/:race_id',
+    name: 'SeatPage',
+    component: SeatPage
+  },
+  {
+    path: '/form/:race_id',
+    name: 'Form',
+    component: form
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    component: AuthorizationPage
+  },
+    {
+    path: '/account',
+    name: 'Account',
+    component: PersonalAccount
+  },
+]
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes
+})
+
+export default router
